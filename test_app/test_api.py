2819208ec16dfd4b3fe694cc21dd73dfad8b6f4c
M4cs <mabridgland@protonmail.com>
2019-12-18 12:55:46 -0500

    Add Test Changes

--- test_app/test_api.py
+++ test_app/test_api.py
@@ -45,6 +45,7 @@ class APITests(unittest.TestCase):
             'password': mock_user.password
         })
         data = json.loads(res.data)
+        print(data)
         self.assertTrue(res.status_code, 200)
         self.assertIsNotNone(data['api-key'])
         mock_user.api_key = data['api-key']
@@ -84,43 +85,6 @@ class APITests(unittest.TestCase):
         for store in data['stores']:
             self.assertEqual(store['owner'], mock_user.email)
         mock_user.count += 1
-            
-    def test_6_create_third_store(self):
-        print_title('Testing Third Store Creation')
-        res = self.app.post('/api_v1/create', headers={
-                'Api-Key': mock_user.api_key,
-                'Content-Type': 'application/json'
-            },
-            data=json.dumps(mock_user.store_template)
-        )
-        data = json.loads(res.data)
-        self.assertTrue(res.status_code, 200)
-        self.assertTrue(data['message'] == 'Success')
-        self.assertIsNotNone(data['name'])
-        mock_user.count += 1
-    
-    def test_7_get_all_stores_with_three(self):
-        print_title('Testing Get All Stores With Three Stores')
-        res = self.app.get('/api_v1/all_stores', headers={'Api-Key': mock_user.api_key})
-        data = json.loads(res.data)
-        self.assertTrue(res.status_code, 200)
-        self.assertEqual(len(data['stores']), 3)
-        for store in data['stores']:
-            self.assertEqual(store['owner'], mock_user.email)
-        mock_user.count += 1
-            
-    def test_8_max_at_three(self):
-        print_title('Testing Max Cap at 3 Stores')
-        res = self.app.post('/api_v1/create', headers={
-                'Api-Key': mock_user.api_key,
-                'Content-Type': 'application/json'
-            },
-            data=json.dumps(mock_user.store_template)
-        )
-        data = json.loads(res.data)
-        self.assertTrue(res.status_code, 403)
-        self.assertTrue(data['error'] == 'Reached 3 store maximum!')
-        mock_user.count += 1
     
     def test_9_edit_store(self):
         print_title('Testing PUT on Store (Edit)')
@@ -136,18 +100,18 @@ class APITests(unittest.TestCase):
         self.assertTrue(res.status_code == 200)
         self.assertNotEqual(mock_user.store_template, data)
     
-    def test_a1_delete_stores(self):
-        print_title('Testing DELETE on Store (Delete)')
-        res = self.app.get('/api_v1/all_stores', headers={'Api-Key': mock_user.api_key})
-        data = json.loads(res.data)
-        for store in data['stores']:
-            res = self.app.delete('/api_v1/stores/{}'.format(store['name']), headers={'Api-Key': mock_user.api_key})
-            self.assertTrue(res.status_code == 200)
-            self.assertTrue(res.json == { 'message': 'Success' })
+    # def test_a1_delete_stores(self):
+    #     print_title('Testing DELETE on Store (Delete)')
+    #     res = self.app.get('/api_v1/all_stores', headers={'Api-Key': mock_user.api_key})
+    #     data = json.loads(res.data)
+    #     for store in data['stores']:
+    #         res = self.app.delete('/api_v1/stores/{}'.format(store['name']), headers={'Api-Key': mock_user.api_key})
+    #         self.assertTrue(res.status_code == 200)
+    #         self.assertTrue(res.json == { 'message': 'Success' })
             
-    def test_a2_clean_up(self):
-        print_title('Cleaning Up Database')
-        mongo.db.free_users.find_one_and_delete({'email': mock_user.email})
+    # def test_a2_clean_up(self):
+    #     print_title('Cleaning Up Database')
+    #     mongo.db.free_users.find_one_and_delete({'email': mock_user.email})
 
 if __name__ == "__main__":
     unittest.main()            
