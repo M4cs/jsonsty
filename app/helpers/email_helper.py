7220f18eb2d89b9bab4862cc2541cd516e8ce2af
M4cs <mabridgland@protonmail.com>
2019-12-19 16:23:28 -0500

    Fix Formatting

--- app/helpers/email_helper.py
+++ app/helpers/email_helper.py
@@ -2,18 +2,18 @@ import dns.resolver
 import re
 
 def verify_email(email):
-		regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
-		if re.search(regex,email):
-			pass
-		else:
-			return False
-		domain = email.split('@')[1]
-		records = []
-		try:
-			for x in dns.resolver.query(domain, 'MX'):
-				records.append(x)
-		except dns.resolver.NoAnswer:
-			return False
-		if len(records) > 0:
-			return True
+	regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+	if re.search(regex,email):
+		pass
+	else:
 		return False
+	domain = email.split('@')[1]
+	records = []
+	try:
+		for x in dns.resolver.query(domain, 'MX'):
+			records.append(x)
+	except dns.resolver.NoAnswer:
+		return False
+	if len(records) > 0:
+		return True
+	return False
