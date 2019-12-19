adb4132108cf8a982161ee200d1d32a2d0dbc3ea
M4cs <mabridgland@protonmail.com>
2019-12-18 19:48:27 -0500

    Clean Up Files

---
+++ app/resources/accounts.py
@@ -0,0 +1,35 @@
+from flask_restful import Resource, reqparse
+from app.models.db_helpers import check_email
+from werkzeug.security import check_password_hash, generate_password_hash
+from app import app, mongo, mhelp, create_chain
+from app.models.crypto_helpers import encrypt_str
+from uuid import uuid4
+from bson import ObjectId
+from datetime import datetime
+
+
+def signup_parser():
+    parser = reqparse.RequestParser()
+    parser.add_argument('email', required=True)
+    parser.add_argument('password', required=True)
+    return parser
+
+class JSONLogin(Resource):
+    def post(self):
+        parser = signup_parser()
+        args = parser.parse_args()
+        result = check_email(args['email'])
+        if not result:
+            return {"error": "No Account with that E-Mail Exists!"}, 403
+        else:
+            pass
+        user = mhelp.get_user({'email': args['email']})
+        if user:
+            if check_password_hash(user.get('password'), args['password']):
+                access_token = str(uuid4())
+                mongo.db.free_users.find_one_and_update({'email': args['email']}, {'$set': { 'current_token': access_token } })
+            else:
+                return { "error": "Incorrect Password!" }, 403
+        else:
+            return {"error": "Something broke"}, 500
+        return {"message": "Logged In!", "api-key": user['api_key']}, 200
